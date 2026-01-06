fb4117852d24acc434173aaabf47939dacdd4570
sameer <sameer.8t1@gmail.com>
2026-01-06 11:27:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+// saying hello
+ /*
+ */
+// console.log("Hello, World!");
+
+let fname = "tiger";
+fname[0] = "1";
+console.log(fname);
+console.log(fname.toUpperCase());
+
+let sayHello = "Hello";
+console.log(sayHello, fname); // Hello tiger
+
+let greet = sayHello +" "+ fname;
+console.log(greet); // Hello tiger
+console.log('${sayHello} ${fname}');
+// let age = 24
+// console.log(typeof fname);
+placement= null;
+console.log(placement);

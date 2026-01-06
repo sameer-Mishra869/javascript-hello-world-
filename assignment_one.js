f28df06d4280c848d57eeec7083fdacf8b9d0582
sameer <sameer.8t1@gmail.com>
2026-01-06 12:10:01 +0530

    Add files via upload

---
+++ assignment_one.js
@@ -0,0 +1,18 @@
+// 1. Greeting message
+console.log("Hello! I'm your coding fun fact guide!");
+
+// 2. Create variables
+const botName = "CodeBuddy";
+const botLocation = "India";
+const favoriteLanguage = "JavaScript";
+
+// 3. Log name and location
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+
+// 4. Log favorite language
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+// 5. Fun fact using let
+let codingFact = `${favoriteLanguage} is awesome because it can be used for both frontend and backend development!`;
+console.log(codingFact);
+

7498284172d3103b87e68379c050aadd17d72794
sameer <sameer.8t1@gmail.com>
2026-01-09 15:03:07 +0530

    Add files via upload

---
+++ datesjb.js
@@ -0,0 +1,12 @@
+let numbers_even = [1, 2, 3, 4, 5, 6];
+let numbers_odd = [1, 2, 3, 4, 5,];
+console.log(numbers_even); // prints entire numbers
+// numbers.length 5 - 1
+console.log(numbers_even[0]); // prints first element of array
+console.log(numbers_even[numbers_even.length - 1]); // prints last element of array
+
+console.log("odd:", numbers_odd[numbers_odd.length / 2]); // middle if odd
+
+console.log("even:", numbers_even[numbers_even.length / 2]); // middle if even (right side)
+
+console.log("even:", numbers_even[numbers_even.length / 2]); // middle if even(left side
